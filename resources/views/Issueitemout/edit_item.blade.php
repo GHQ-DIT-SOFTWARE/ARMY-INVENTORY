4764b37c8796144a24b327e30f829fcb99872f64
Nana Prof <nanakeen85@gmail.com>
2024-06-22 21:51:24 +0000

    made some changes

--- resources/views/Issueitemout/edit_item.blade.php
+++ resources/views/Issueitemout/edit_item.blade.php
@@ -7,7 +7,7 @@
             <div class="row align-items-center">
                 <div class="col-md-12">
                     <div class="page-header-title">
-                        <h5 class="m-b-10">Item </h5>
+                        <h5 class="m-b-10">Item Issue Voucher </h5>
                     </div>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a></li>
